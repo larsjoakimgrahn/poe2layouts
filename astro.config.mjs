2ff2856f64ac69aaea0edc393288f3cb1182dde0
Joakim Grahn <joakim.grahn@qapital.com>
2025-03-29 17:51:14 +0100

    Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
     site: "https://larsjoakimgrahn.github.io/poe2layouts/",
+    base: "/poe2layouts/",
     vite: {
         plugins: [tailwindcss()],
     },
